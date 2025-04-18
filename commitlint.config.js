6af845c6ca382c54a0e4cbb48393c1c323b6f6fb
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2025-04-17 22:16:14 -0600

    build(repo): update commitlint configuration
    Accept the `deps` scope
    
    FIX add test project' scopes

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,14 +1,17 @@
 import { readdir } from 'node:fs/promises';
 import { basename } from 'node:path';
 
-const DEFAULT_SCOPES = ['repo', 'changeset'];
+const DEFAULT_SCOPES = ['repo', 'changeset', 'deps'];
 const apps = (await readdir('apps', { withFileTypes: true }))
   .filter((entry) => entry.isDirectory())
   .map((entry) => basename(entry.name));
 const packages = (await readdir('packages', { withFileTypes: true }))
   .filter((entry) => entry.isDirectory())
   .map((entry) => basename(entry.name));
-const scopes = [...DEFAULT_SCOPES, ...apps, ...packages];
+const tests = (await readdir('tests', { withFileTypes: true }))
+  .filter((entry) => entry.isDirectory())
+  .map((entry) => basename(entry.name));
+const scopes = [...DEFAULT_SCOPES, ...apps, ...packages, ...tests];
 
 export default {
   extends: ['@commitlint/config-conventional', 'monorepo'],
